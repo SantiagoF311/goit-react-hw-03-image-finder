a98729c2783cac0d51e4c688e49b648407db4472
Santiago <santiagofonsecaf311@gmail.com>
2023-05-24 21:54:26 -0500

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,13 @@
+import React, { Component } from "react";
+
 export const App = () => {
+
+  state = {
+    
+  }
+
+  
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <></>
   );
 };
